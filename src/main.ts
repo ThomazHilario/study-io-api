70bab3d028c417185ced138162bbf7c70039025e
Thomaz Alves <thomazhilario5@gmail.com>
2024-09-04 14:18:48 -0300

    feat(main.ts):cors
    - Habilitando o cors a aplicação.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,12 @@ import { ValidationPipe } from '@nestjs/common';
 import { EmailModule } from './email/email.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(EmailModule);
+  const app = await NestFactory.create(EmailModule, {cors:true});
   
+  // Active cors
+  app.enableCors()
+
+  // Global Pipes
   app.useGlobalPipes(
     new ValidationPipe()
   )
