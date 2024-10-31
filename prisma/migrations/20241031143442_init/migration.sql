d879b57bdf231a43e1fc3094e12170377fd52ad5
Thomaz Alves <thomazhilario5@gmail.com>
2024-10-31 11:35:33 -0300

    Create route themes

---
+++ prisma/migrations/20241031143442_init/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "Themes" (
+    "id" TEXT NOT NULL,
+    "image_url" TEXT NOT NULL,
+    "video_url" TEXT NOT NULL,
+
+    CONSTRAINT "Themes_pkey" PRIMARY KEY ("id")
+);
--- src/main.module.ts
+++ src/main.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { EmailModule } from "./email/email.module";
+import { ThemesModule } from './themes/themes.module';
 
 @Module({
-    imports:[EmailModule],
+    imports:[EmailModule, ThemesModule],
 })
 
 export class MainModule{}
