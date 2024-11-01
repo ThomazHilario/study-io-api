d43fe2ddd06ebd321a5548195bd750ae8e6a01a1
Thomaz Alves <thomazhilario5@gmail.com>
2024-10-31 22:28:10 -0300

    import prisma service in mainmodule

--- src/main.module.ts
+++ src/main.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { EmailModule } from "./email/email.module";
 import { ThemesModule } from './themes/themes.module';
+import { PrismaService } from "./prisma.service";
 
 @Module({
     imports:[EmailModule, ThemesModule],
+    providers:[PrismaService],
+    exports:[PrismaService]
 })
 
 export class MainModule{}
