7797603309ee4a0f132aed0da0d46e0179c2cbd1
Thomaz Alves <thomazhilario5@gmail.com>
2024-11-01 18:12:17 -0300

    feat: Verificação do usuário
    - Caso um admin adicione um theme, a função insertThemes irá verificar se o usuário é um admin ou não.

--- src/themes/themes.repository.ts
+++ src/themes/themes.repository.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@nestjs/common'
 
+// Firebase
+import * as Firebase from 'firebase-admin'
+
 // Prisma
 import { PrismaService } from 'src/prisma.service'
 
 @Injectable()
 
 export class ThemesRepository{
+
+    // firebase collection
+    firebase:FirebaseFirestore.CollectionReference = Firebase.firestore().collection('users')
+
+    // Constructor
     constructor(private prisma:PrismaService){}
 
     // Return all themes
@@ -16,22 +24,43 @@ export class ThemesRepository{
     // Insert themes
     async insertTheme(image_url:string, video_url:string){
         try {
-            // Create new theme
-            await this.prisma.themes.create({
-                data:{
-                    image_url,
-                    video_url,
-                }
-            })
 
-            // Return message
+            // allUsers
+            let allUsers = []
+
+            // Find All users from firebase
+            const usersFromFirebase = await this.firebase.get()
+
+            // Push user in allUsers
+            usersFromFirebase.forEach(user => allUsers.push(user.data()))
+
+            // Get admin verification
+            const verifiedUser = allUsers.find(user => user.dataUser.email === process.env.TOKEN)
+
+            // Logic case is verified admin
+            if(verifiedUser){
+                // Create new theme
+                await this.prisma.themes.create({
+                    data:{
+                        image_url,
+                        video_url,
+                    }
+                })
+
+                // Return message
+                return {
+                    message: 'Created theme in database'
+                }
+            }
+
+            // Erro message
             return {
-                message: 'Created theme in database'
+                message:'You not is verified',
             }
 
         } catch (error) {
             return{
-                message:'Error! Not is possible create theme'
+                message:'Error! Not is possible create theme',
             }
         }
             
